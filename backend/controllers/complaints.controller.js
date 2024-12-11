c6418e1527a186db765e433737648c1f4eddeea4
Mihir Bairathi <mihirbairathi25@gmail.com>
2024-12-11 17:32:05 +0530

    updated the complait registing api

--- backend/controllers/complaints.controller.js
+++ backend/controllers/complaints.controller.js
@@ -4,6 +4,8 @@ import pnrData from "../models/pnrData.js"
 import trainData from "../models/trains.js"
 import employeeData from "../models/employee.js"
 import putData from "../test.js"
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
 
 let ticketid=1234567321
 
@@ -23,9 +25,20 @@ export const createComplaint= async (req, res) => {
     const Phone=req.body.mobileNo
     const complaintMedia=req.body.files
     const complaintDesc=req.body.grievanceDescription
-    console.log(req.body);
+
     const loggedin=req.loggedin
     let user;
+
+    function generateRandomPassword(length = 15) {
+        const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+        let password = '';
+        for (let i = 0; i < length; i++) {
+          const randomIndex = Math.floor(Math.random() * charset.length);
+          password += charset[randomIndex];
+        }
+        return password;
+      }
+
     if(loggedin){
         user = await User.findOne({_id:req.user._id});
         if(!user){
@@ -36,16 +49,18 @@ export const createComplaint= async (req, res) => {
         user = await User.findOne({phone:Phone});
     }
     if(!user){
+        const hashPassword = await bcryptjs.hash(generateRandomPassword(),10)
         user = await User.create({
             phone:Phone,
-            password: "RANDOML15DIGITSTRING",
+            password: hashPassword,
             name:"DATA FROM PNR",
             complaintTickets: [],
             email:"test@gmail.com"
         }) 
     }
+
    const dataFromPnr=await pnrData.findOne({pnrNumber:PNR})
-   console.log(dataFromPnr)
+//    console.log(dataFromPnr)
     let newComplaint= new Complaints({
         complaintId:ticketid,
         phone: Phone,
@@ -65,13 +80,13 @@ export const createComplaint= async (req, res) => {
         resolved:0,
         severity:0
     })
+
     await newComplaint.save()
-    console.log("donetop")
     let currentId=ticketid
     ticketid=ticketid+1
     user.complaintTickets.push(currentId)
-    user.save()
-    console.log("done2")
+    await user.save()
+    // console.log("done2")
     let category=""
     let subCategory=""
     let severity=0
@@ -81,26 +96,45 @@ export const createComplaint= async (req, res) => {
     severity=2
     //
     if(category!=""){
-        console.log("done1")
+        // console.log("done1")
         let dataFromTrain=await trainData.findOne({$and:[
             {trainCode:dataFromPnr.trainCode},
             {trainDepartureDate:dataFromPnr.trainDepartureDate}
         ]})
         // let defa=await
-        console.log(dataFromPnr)
-        console.log(dataFromTrain)
+        // console.log(dataFromPnr)
+        // console.log(dataFromTrain)
         // return res.status(200)
         let employeeId=dataFromTrain[category][subCategory]
         await Complaints.findOneAndUpdate(
             { complaintId: currentId }, 
             { $set: { category: category,subCategory:subCategory,employeeWorking:employeeId,severity:severity } })
-        console.log("done3")
+        // console.log("done3")
         const emp=await employeeData.findOne({employeeId:employeeId})
         emp.complaints.push(currentId)
         emp.save()
-        
-        res.json({ message: 'Complaint Registered Succesfully'});
+        const options = {
+            expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+             //only manipulate by server not by client/user
+             secure:false,
+             httpOnly:true
+        };
+        const token = jwt.sign({_id: user._id }, process.env.SECRET_KEY, {
+                expiresIn: "1d",
+            
+            });
+        if(loggedin){
+            return res.status(200).json({ message: 'Complaint Registered Succesfully'});
+
+        }
+        return res.cookie("acessToken", token,options).status(200).json({ message: 'Complaint Registered Succesfully'});
     }
-    else {res.status(500).json({ message: 'Could not Register complaint,Complaint is saved but '});}
+    else { 
+        if(loggedin){
+            return res.status(500).json({ message: 'Complaint Registered Succesfully'});
+
+        }
+        return res.cookie("acessToken", token,options).status(500).json({ message: 'Could not Register complaint,Complaint is saved but '});
+    }
         
 };
